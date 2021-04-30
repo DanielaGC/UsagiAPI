f498c668b223f8987dd80415317105e346ac8ec7
DanielaGC <danielagc676@gmail.com>
2021-04-30 17:06:52 -0300

    Adding new functions

--- src/UsagiAPIClient.js
+++ src/UsagiAPIClient.js
@@ -1,37 +1,47 @@
 const axios = require('axios')
 
 module.exports = class UsagiAPIClient {
-    constructor() {
-        this.url = 'https://usagiapi.danielagc.repl.co/api/'
-    }
+  constructor() {
+    this.url = 'https://usagiapi.danielagc.repl.co/api/'
+  }
 
-    async dance() {
-        const res = await axios.get(`${this.url}/dance`)
-        return res.data.url
-    }
+  async dance() {
+    const res = await axios.get(`${this.url}/dance`)
+    return res.data.url
+  }
 
-    async feed() {
-        const res = await axios.get(`${this.url}/feed`)
-        return res.data.url
-    }
+  async feed() {
+    const res = await axios.get(`${this.url}/feed`)
+    return res.data.url
+  }
 
-    async hug() {
-        const res = await axios.get(`${this.url}/hug`)
-        return res.data.url
-    }
+  async hug() {
+    const res = await axios.get(`${this.url}/hug`)
+    return res.data.url
+  }
 
-    async kiss() {
-        const res = await axios.get(`${this.url}/kiss`)
-        return res.data.url
-    }
+  async kiss() {
+    const res = await axios.get(`${this.url}/kiss`)
+    return res.data.url
+  }
 
-    async slap() {
-        const res = await axios.get(`${this.url}/slap`)
-        return res.data.url
-    }
+  async pat() {
+    const res = await axios.get(`${this.url}/pat`)
+    return res.data.url
+  }
 
-    async tickle() {
-        const res = await axios.get(`${this.url}/tickle`)
-        return res.data.url
-    }
+  async poke() {
+    const res = await axios.get(`${this.url}/poke`)
+    return res.data.url
+  }
+
+  async slap() {
+    const res = await axios.get(`${this.url}/slap`)
+    return res.data.url
+  }
+
+  async tickle() {
+    const res = await axios.get(`${this.url}/tickle`)
+    return res.data.url
+  }
 }
