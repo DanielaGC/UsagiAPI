7432c90bb810ddb12097172bb7bf170310df85ca
DanielaGC <danielagc676@gmail.com>
2021-04-30 17:07:34 -0300

    1.0.2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    UsagiAPI: require('./src/UsagiAPIClient'),
-    version: require('./package.json').version
+  UsagiAPI: require('./src/UsagiAPIClient'),
+  version: require('./package.json').version
 }
