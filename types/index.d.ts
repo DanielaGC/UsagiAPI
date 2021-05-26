76c78aa0426d72f6e5292afd4c9c6cd2e40f5b71
Yslan Ramos <yslanramos.r@gmail.com>
2021-05-26 16:08:04 -0300

    fix types

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,13 +2,14 @@
 declare module 'usagiapi' {
     export class UsagiAPIClient {
         url: string;
-        async dance(): Promise<string>;
-        async feed(): Promise<string>;
-        async hug(): Promise<string>;
-        async kiss(): Promise<string>;
-        async pat(): Promise<string>;
-        async poke(): Promise<string>;
-        async slap(): Promise<string>;
-        async tickle(): Promise<string>;
+        _request(routerName: string): Promise<string>;
+        dance(): Promise<string>;
+        feed(): Promise<string>;
+        hug(): Promise<string>;
+        kiss(): Promise<string>;
+        pat(): Promise<string>;
+        poke(): Promise<string>;
+        slap(): Promise<string>;
+        tickle(): Promise<string>;
     }
 }
