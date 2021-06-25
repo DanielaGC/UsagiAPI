41cf233b782d8d5e2607bbb50b5a9d9cd6a95cd3
DanielaGC <danielagc676@gmail.com>
2021-06-25 02:01:27 -0300

    Adding .get function

--- src/UsagiAPIClient.js
+++ src/UsagiAPIClient.js
@@ -11,34 +11,54 @@ module.exports = class UsagiAPIClient {
   }
 
   dance() {
+    console.log('[DEPRECATED] - The dance functions is deprecated, use .get({ endpoint: \'dance\' })')
     return this._request('dance');
   }
 
   feed() {
+    console.log('[DEPRECATED] - The feed functions is deprecated, use .get({ endpoint: \'feed\' })')
     return this._request('feed');
   }
 
- hug() {
+  get(option) {
+    if (typeof option !== 'object') {
+      throw new TypeError(`[INVALID-TYPE] - This option needs to be a object, try .get({ endpoint: '${option.join('')}' })`)
+    }
+
+    if (!option && !option.endpoint) {
+      throw new TypeError('[INVALID-OPTION] Set as endpoints options to make a request.')
+    }
+
+    return this._request(option.endpoint)
+  }
+
+  hug() {
+    console.log('[DEPRECATED] - The hug functions is deprecated, use .get({ endpoint: \'hug\' })')
     return this._request('hug');
   }
 
- kiss() {
+  kiss() {
+    console.log('[DEPRECATED] - The kiss functions is deprecated, use .get({ endpoint: \'kiss\' })')
     return this._request('kiss');
   }
 
- pat() {
+  pat() {
+    console.log('[DEPRECATED] - The pat functions is deprecated, use .get({ endpoint: \'pat\' })')
     return this._request('pat');
   }
 
- poke() {
+  poke() {
+    console.log('[DEPRECATED] - The poke functions is deprecated, use .get({ endpoint: \'poke\' })')
     return this._request('poke');
   }
 
- slap() {
+  slap() {
+    console.log('[DEPRECATED] - The slap function is deprecated, use .get({ endpoint: \'slap\' })')
     return this._request('slap');
   }
 
- tickle() {
+  tickle() {
+    console.log('[DEPRECATED] - The tickle functions is deprecated, use .get({ endpoint: \'tickle\' })')
     return this._request('tickle');
   }
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,15 +1,15 @@
-
 declare module 'usagiapi' {
-    export class UsagiAPIClient {
-        url: string;
-        _request(routerName: string): Promise<string>;
-        dance(): Promise<string>;
-        feed(): Promise<string>;
-        hug(): Promise<string>;
-        kiss(): Promise<string>;
-        pat(): Promise<string>;
-        poke(): Promise<string>;
-        slap(): Promise<string>;
-        tickle(): Promise<string>;
-    }
+  export class UsagiAPIClient {
+    url: string;
+    _request(routerName: string): Promise<string>;
+    dance(): Promise<string>;
+    feed(): Promise<string>;
+    hug(): Promise<string>;
+    get(option: object): Promise<string>;
+    kiss(): Promise<string>;
+    pat(): Promise<string>;
+    poke(): Promise<string>;
+    slap(): Promise<string>;
+    tickle(): Promise<string>;
+  }
 }
